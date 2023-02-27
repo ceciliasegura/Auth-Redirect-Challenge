7420b647114d25a26854552ae2263217e864a706
ceci <ceciliasegura.t@hotmail.com>
2023-02-27 22:50:46 +0100

    to do post

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,10 @@
+import bosque from '../../assets/banner.png'
 export default function Header(){
     return(
         <>
-        <img src="./assets/banner.png" alt="imagen banner" />
+        <img src={bosque} className="w-full h-[192px]" alt="imagen banner" />
         <div className="flex items-center justify-center">
-        <div className="  w-[132px] h-[132px] bg-white border-4 rounded-full border-green-color"></div>
+        <div className="  w-[132px] h-[132px] bg-white border-4 rounded-full border-green-color absolute"></div>
         </div>
         </>
     )
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,16 +1,33 @@
+
 export default function Login() {
-    return (
+    const token="https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyCOZiKCWqYYjx2ZZ02Y9UZmMPbfcbo6h3Q"
+
+    const handleSubmit= (event) =>{
+        event.preventDefault();
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: 'fran.cadenas+officer@fusuma.io' , password: '123456', returnSecureToken:'true' })
+        };
+        fetch(token, requestOptions)
+            .then(response => response.json())
+            .then(result => console.log(result) );
+    
+    };
+     
+     return (
         <>
             <div className=" flex justify-center items center">
 
-                <p className="text-3xl	font-extrabold	text-black-color flex flex-col justify-center items-center pt-6 mb-6 ">Iniciar sesión</p>
+                <p className="text-3xl	font-extrabold	text-black-color flex flex-col justify-center items-center pt-20 mb-6 ">Iniciar sesión</p>
             </div>
 
             <div className="flex justify-center items center">
 
                 <div className=" flex justify-center items center w-[448px] h-[282px] bg-white pt-10">
 
-                    <form action="" className="py-8 px-10 flex flex-col gap-6 w-[448px]">
+                    <form onSubmit={handleSubmit} className="py-8 px-10 flex flex-col gap-6 w-[448px]">
                         <div className="flex flex-col gap-1 mt-[-20px]">
                             <label htmlFor="email" className="text-black-color font-medium text-sm">Email</label>
                             <input type="text" name="email" id="email" className="'py-[9px] px-[13px] rounded-md border border-grey-color" />
